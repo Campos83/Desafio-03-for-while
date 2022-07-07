c9fcf855732bf119bdb7c63938bcbf2e23c0c7eb
Willian Campos <Campos83>
2022-07-07 16:26:56 -0300

    Desafio-03-for-while

---
+++ script.js
@@ -0,0 +1,16 @@
+for(let number =1; number <= 10; number ++){
+    console.log(number);
+}
+
+let number=10
+while(number >=1){
+    console.log(number--);
+    }
+
+for(let number =1; number <= 100; number +=2){
+    console.log(number);
+}
+  
+for(let number =0; number >= 100; number +=2){
+    console.log(number);
+}
